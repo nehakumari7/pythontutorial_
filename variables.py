173fe0d83e0b04999591d576326d4c7eb43dfaa9
nehakumari7 <58387771+nehakumari7@users.noreply.github.com>
2019-12-01 17:00:10 +0530

    some

--- variables.py
+++ variables.py
@@ -6,7 +6,7 @@ if __name__=='__main__':
     c=a+b
     print("Answer " + str(c))
     print(c)
-    '''
+    '''''''''
     a=input("enter value of a ")
     b=input("enter value of b ")
     c=int(a) + int(b)
